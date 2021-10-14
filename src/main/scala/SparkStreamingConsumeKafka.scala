634aecc87f87d11f61bf1e403e9c16e752ff4b56
senth <ksenthil84@gmail.com>
2021-10-14 19:17:32 +0530

    first commit

---
+++ src/main/scala/SparkStreamingConsumeKafka.scala
@@ -0,0 +1,35 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{explode, split}
+
+object SparkStreamingConsumeKafka {
+
+  def main(args: Array[String]): Unit = {
+
+    val spark:SparkSession = SparkSession.builder()
+      .master("local[3]")
+      .appName("SparkByExample")
+      .getOrCreate()
+
+    spark.sparkContext.setLogLevel("ERROR")
+
+    val df = spark.readStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", "localhost:9092")
+      .option("subscribe", "json_data_topic")
+      //.option("subscribePattern", "topic.*")
+      .option("startingOffsets", "earliest") // Other possible values assign and latest
+      .load()
+
+    df.printSchema()
+
+    val groupCount = df.select(explode(split(df("value")," ")).alias("word"))
+      .groupBy("word").count()
+
+    groupCount.writeStream
+      .format("console")
+      .outputMode("complete")
+      .start()
+      .awaitTermination()
+
+  }
+}
